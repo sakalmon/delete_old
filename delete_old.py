e70021abf41b0918c1701dfced05d4e5b93ad18d
Sakal Mon <sakalathmon@gmail.com>
2021-04-09 19:13:06 +1000

    Delete files that are over 14 days old

--- delete_old.py
+++ delete_old.py
@@ -35,13 +35,13 @@ for file in os.listdir(path):
             file_to_delete = os.path.join(path, file)            
 
             if os.path.exists(file_to_delete):                
-                #os.remove(file_to_delete)
+                os.remove(file_to_delete)
                 print(file_to_delete + ' deleted.')
 
             mbytes_deleted += os.stat(file_to_delete).st_size / 1000000 # Get size of file and convert to Megabytes
             mbytes_deleted = round(mbytes_deleted)
 
-print(f'{count} files deleted ({mbytes_deleted}MB).')
+print(f'{count} files deleted ({mbytes_deleted}MB)')
 
 
 
